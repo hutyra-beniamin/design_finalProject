f59e1956689bea33aa7389c7e6d0d8d78315b17b
hutyra.beniamin <hutyra.beniamin@outlook.com>
2020-08-12 10:42:06 +0200

    first commit

---
+++ version1.0/fproject/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { SignedComponent } from './signed/signed.component';
+import { SignComponent } from './sign/sign.component';
+import { SocialComponent } from './social/social.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    AboutComponent,
+    BlogComponent,
+    SignedComponent,
+    SignComponent,
+    SocialComponent,
+    ContactComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ version1.0/fproject/src/app/signed/signed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signed',
+  templateUrl: './signed.component.html',
+  styleUrls: ['./signed.component.css']
+})
+export class SignedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
